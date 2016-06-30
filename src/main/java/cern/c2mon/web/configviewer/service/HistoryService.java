a3c1a2b85efa0b1d49e6165da5c6ec5c62678a3c
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-06-30 15:14:23 +0200

    Fix boolean data detection

--- src/main/java/cern/c2mon/web/configviewer/service/HistoryService.java
+++ src/main/java/cern/c2mon/web/configviewer/service/HistoryService.java
@@ -361,9 +361,8 @@ public class HistoryService {
     final int size = historyValues.size();
     if (size > 0) {
       final HistoryTagValueUpdate h = historyValues.iterator().next();
-      final String type = h.getDataType();
-      final boolean isBooleanData = type.equals("Boolean");
-      return isBooleanData;
+      final String type = h.getValueClassName();
+      return type.contains("Boolean");
     }
     return false;
   }
