3bd57db0b96b0107b73f0151f5531d4eec93089a
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-10-22 22:20:05 +0200

    Add webpack config

---
+++ src/main/frontend/config/webpack-prod.js
@@ -0,0 +1,32 @@
+var loaders = require('./webpack-loaders');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main/frontend/app/app.ts',
+    output: {
+        path: './target/classes/static/bin',
+        filename: 'app.js'
+    },
+    resolve: {
+        modulesDirectories: ['./node_modules'],
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+        alias: {
+            'ui-router': 'angular-ui-router/release/angular-ui-router.js'
+        }
+    },
+    module: {
+        loaders: loaders
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash',
+            moment: 'moment',
+            $: 'jquery',
+            jQuery: 'jquery',
+            latinize: 'latinize'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ]
+};
