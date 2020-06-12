4cca35595b2870e0b63a23b30038c9d6a370f3de
bcopy <500789+bcopy@users.noreply.github.com>
2020-06-12 18:48:50 +0200

    CM-267 Switch to executable WAR packaging for JSP support

--- src/main/java/cern/c2mon/web/ui/Application.java
+++ src/main/java/cern/c2mon/web/ui/Application.java
@@ -3,14 +3,14 @@ package cern.c2mon.web.ui;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 /**
  * @author Justin Lewis Salmon
  */
 @SpringBootApplication
-public class Application {
-
-  public static void main(String[] args) throws Exception {
+public class Application extends SpringBootServletInitializer {
+	
+  public static void main(String[] args) {
     new SpringApplicationBuilder().bannerMode(Banner.Mode.OFF).sources(Application.class).run(args);
   }
 }
--- src/main/java/cern/c2mon/web/ui/config/StatisticsDataSourceConfig.java
+++ src/main/java/cern/c2mon/web/ui/config/StatisticsDataSourceConfig.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
@@ -24,11 +26,15 @@ public class StatisticsDataSourceConfig {
   private Environment environment;
 
   @Bean
-  public DataSource statisticsDataSource() {
-    return DataSourceBuilder.create()
-        .url(environment.getProperty("c2mon.web.statistics.jdbc.url"))
-        .username(environment.getProperty("c2mon.web.statistics.jdbc.username"))
-        .password(environment.getProperty("c2mon.web.statistics.jdbc.password")).build();
+  @ConfigurationProperties("c2mon.web.statistics.jdbc")
+  public DataSourceProperties statisticsDataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  @Bean
+  @ConfigurationProperties("c2mon.web.statistics.jdbc")
+  public DataSource statisticsDataSource(DataSourceProperties statisticsDataSourceProperties) {
+    return statisticsDataSourceProperties.initializeDataSourceBuilder().build();
   }
 
   @Bean
